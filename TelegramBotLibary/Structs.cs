1a3466f992df9f5da56bd999be0cffebf31749f0
Alleshka <grebovod_aleksei@mail.ru>
2017-07-19 14:28:00 +0300

    Основные структуры... такое можно уже улучшать

---
+++ TelegramBotLibary/Structs.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBotLibary
+{
+
+    public class Result
+    {
+        public int _update_id { get; set; }
+        public Message _message { get; set; }
+    }
+
+    // Этот объект представляет собой сообщение.
+    public class Message
+    {
+        public int _message_id { get; set; } // Уникальный идентификатор сообщения
+        public User _from { get; set; } // Отправитель
+        public int _date { get; set; } // Дата UnixTime
+
+        public Chat _chat { get; set; } // Диалог, в котором отправлено сообщение
+
+        public String _text { get; set; } // Опционально. Для текстовых сообщений: текст сообщения, 0-4096 символов
+    }
+
+    // Этот объект представляет бота или пользователя Telegram.
+    public class User
+    {
+        public int _id { get; set; } // Уникальный идентификатор пользователя или бота
+        public String _first_name { get; set; } // Имя бота или пользователя
+        public String _last_name { get; set; } // Опционально. Фамилия бота или пользователя
+        public String _username { get; set; } // Опционально. Username пользователя или бота
+
+        public User(int id, String first, String last, String username)
+        {
+            _id = id;
+            _first_name = first;
+            _last_name = last;
+            _username = username;
+        }
+    }
+
+    // Этот объект представляет собой чат.
+    public class Chat
+    {
+        public int _id { get; set; } // Уникальный идентификатор чата. Абсолютное значение не превышает 1e13
+        public String _type { get; set; } // Тип чата: “private”, “group”, “supergroup” или “channel”
+
+        public String _title { get; set; } // Опционально. Название, для каналов или групп
+
+        public String _username { get; set; } // 	Опционально. Username, для чатов и некоторых каналов
+
+        public String _first_name { get; set; } // Опционально. Имя собеседника в чате
+
+        public String _last_name { get; set; } // Опционально. Фамилия собеседника в чате
+
+        public bool all_members_are_administrators { get; set; } // Опционально.True, если все участники чата являются администраторами
+
+        public Chat(int id, String type, String title, String name, String first, String last)
+        {
+            _id = id;
+            _type = type;
+            _title = _title;
+            _username = name;
+            _first_name = first;
+            _last_name = last;
+        }
+    }
+}
---
+++ TelegramBotLibary/TelegramBot.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.Collections.Specialized;
+using Newtonsoft;
+using Newtonsoft.Json.Linq;
+
+namespace TelegramBotLibary
+{
+    public class TelegramBot
+    {
+        protected string _Token; // Секретный ключ
+        protected int _LastUpdateID; // Последнее обновление
+
+        protected List<Result> _results; // Список входящих сообщений
+
+        public TelegramBot(string Token, int UpdateID)
+        {
+            this._Token = Token;
+            this._LastUpdateID = UpdateID;
+
+            _results = new List<Result>();
+        }
+
+        // Получаем обновления через API
+        public string GetUpdates()
+        {
+            string response = "";
+
+            using (var webClient = new WebClient())
+            {
+                // Получили неотвеченые сообщения в формате JSON
+                response = webClient.DownloadString("https://api.telegram.org/bot" + _Token + "/getUpdates" + "?offset=" + (_LastUpdateID + 1));
+
+                // Достаём ответы и помещаем в список
+                ParseResponce(ref response);
+            }
+
+            return response;
+        }
+
+        private void ParseResponce(ref string response)
+        {
+            // Поехали парсить
+            JObject oneObject = JObject.Parse(response); // Спарсили
+
+            JArray respondItem = (JArray)oneObject["result"]; // Достали ответы
+
+            // Пробегаем по полученным результатам
+            foreach (JObject t in respondItem)
+            {
+                try
+                {
+                    JObject message = (JObject)t["message"]; // Достали сообщение
+
+                    // Достаём id сообщния 
+                    int msgid = (int)message["message_id"];
+
+                    User fromUs = GetInfoFrom(ref message); // Достали инфу о юзере
+                    Chat ChatUs = GetInfoChat(ref message); // Достали инфу о чате
+
+                    // Достаём дату
+                    int date = (int)message["date"];
+
+                    // Достаём текст
+                    String text = (String)message["text"];
+
+                    Message msg = new Message();
+                    msg._message_id = msgid;
+                    msg._from = fromUs;
+                    msg._chat = ChatUs;
+                    msg._date = date;
+                    msg._text = text;
+
+                    _results.Add(new Result { _update_id = (int)t["update_id"], _message = msg });
+                    _LastUpdateID = (int)t["update_id"];
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+
+        // Достаём информацию об отправителе
+        private User GetInfoFrom(ref JObject message)
+        {
+            JObject from = (JObject)message["from"]; // Информация об отправителе
+            User fromUs = new User((int)from["id"], (string)from["first_name"], (string)from["last_name"], (string)from["username"]);
+            return fromUs;
+        }
+        private Chat GetInfoChat(ref JObject message)
+        {
+            JObject chat = (JObject)message["chat"]; // Информация о чате
+
+            int chatId = (int)chat["id"];
+            string title = (string)chat["title"]; // Опционально
+            String username = (String)chat["username"];
+            String first = (String)chat["first_name"];
+            String last = (String)chat["last_name"];
+
+            return new Chat(chatId, "private", title, username, first, last);
+        }
+
+        public List<Result> GetResult()
+        {
+            return _results;
+        }
+    }
+}
