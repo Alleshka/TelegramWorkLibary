bcdae830c9b5466c9ba43e57bf186f0a5c53d8f5
Alleshka <grebovod_aleksei@mail.ru>
2017-08-24 00:41:17 +0300

    Добавил остальные классы. Надо перепроверить

---
+++ New/TelegramWorkLibrary/TelegramWorkLibrary/Struct/KeyboardButton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramWorkLibrary.Struct
+{
+    struct KeyboardButton
+    {
+        public String _text { get; set; } // Текст кнопки
+
+        //Опционально. Если значение True, 
+        //то при нажатии на кнопку боту отправится контакт пользователя с его номером телефона.
+        //Доступно только в диалогах с ботом.
+        public bool _requestContact { get; set; }
+
+        //Опционально.Если значение True, 
+        //то при нажатии на кнопку боту отправится местоположение пользователя.
+        //Доступно только в диалогах с ботом.
+        public bool _requestLocation { get; set; } 
+    }
+}
---
+++ New/TelegramWorkLibrary/TelegramWorkLibrary/Struct/ReplyKeyboardHide.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramWorkLibrary.Struct
+{
+    struct ReplyKeyboardHide
+    {
+        public bool _hideKeyboard { get; set; } // указание клиенты скрыть клавиатуру бота 
+        public bool _selective { get; set; } // Скрыть клавиатуру для определённых пользователей
+    }
+}
